2c24b1ce0459c9a8cbf4939a97575debd6e6f828
Kotin-for-win <mreeve27@igs.vic.edu.au>
2022-10-11 13:35:12 +1100

    Fix bug in duckpowered.sh

--- duckpowered.sh
+++ duckpowered.sh
@@ -94,6 +94,7 @@ do
 		done
 	fi
 done
+elif [ "$1" = "install" ]; then 
 echo "Installing DuckPowered v0.1a..."
 $SNAP/cargo/zenity --info --width 300 --text "Welcome to the DuckPowered install wizard. Press 'OK' to continue..."
 echo "Stage [1/2] Configuring local storage..."
