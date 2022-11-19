578540fa777b4531e4bf0d587e75d90bc27e8874
Kotin-for-win <mreeve27@igs.vic.edu.au>
2022-11-19 18:12:44 +1100

    Version 1.1c (PowerPC Pre-build)
    What's new in 1.1c:
    
    💼Legal stuff about giving proper credit fixed
    🦆The DuckPowered app now shows our logo in the tray
    
    ⚡This means that we are GO for global release⚡

---
+++ NOTICE.md
@@ -0,0 +1,25 @@
+# Legally Required Credits for Open Source Projects we have used:
+*These wered used for the HTML GUI*
+
+**Tauri App**
+
+Thanks to Tauri (https://tauri.app) allowing our HTML GUI to run through GTK.
+
+It's licensed under Apache 2.0, which you can find here: https://www.apache.org/licenses/LICENSE-2.0
+
+We have not modified it.
+
+Code from Tauri is used in gui/src-tauri/* with the exeception of gui/src-tauri/src/* and src-tauri/icons/*
+
+**GaugeSVG**
+
+
+Thanks to GaugeSVG by Steffen Ploetz (https://https://www.codeproject.com/Articles/604502/A-Universal-Gauge-for-Your-Web-Dashboard).
+
+This allows the 'CPU Usage' gauge on our dashboard.
+
+It's licensed under the LGPL, which you an find here: https://www.gnu.org/licenses/lgpl-3.0.en.html
+
+We have not modified it.
+
+Code from GauageSVG is used in gui/src/javascript/*
--- gui/gui-launcher.sh
+++ gui/gui-launcher.sh
@@ -12,9 +12,26 @@ FailureCase () {
     CliPrint
 }
 if [ "$1" = "" ]; then
+    echo "DuckPowered Graphical Dashboard is based on another software."
+    echo "Please type 'duckpowered credits' to learn more."
     $SNAP/usr/bin/dash || FailureCase
 elif [ "$1" = "cli" ]; then
     CliPrint
+elif [ "$1" = "credits" ]; then
+    echo "Thanks to the following people who make DuckPowered a reality:"
+    echo "Harvey, Jai, Neve, Alister, Michael and Ms Hin"
+    echo "Special shoutout to Zoe and Shalamanda, your fun duck antics kept us going!"
+    echo "==========================================================================="
+    echo "Legally Required Credits for Open Source Projects we have used:"
+    echo "==== Tauri App ==== "
+    echo "Thanks to Tauri (https://tauri.app) allowing our HTML GUI to run through GTK"
+    echo "It's licensed under Apache 2.0, which you can find here: https://www.apache.org/licenses/LICENSE-2.0"
+    echo "We have not modified it."
+    echo "==== GaugeSVG  ==== "
+    echo "Thanks to GaugeSVG by Steffen Ploetz (https://https://www.codeproject.com/Articles/604502/A-Universal-Gauge-for-Your-Web-Dashboard)"
+    echo "This allows the 'CPU Usage' gauge on our dashboard."
+    echo "It's licensed under the LGPL, which you an find here: https://www.gnu.org/licenses/lgpl-3.0.en.html"
+    echo "We have not modified it."
 else
     echo "Error - unknown argument"
 fi
